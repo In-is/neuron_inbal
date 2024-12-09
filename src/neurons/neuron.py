bcc58a62ae8c97eef2640ca527a6a51367a0a394
Inbal Israeli Gaffa <inbalisga@gmail.com>
2024-12-10 01:15:01 +0200

    almost final solution

--- src/neurons/neuron.py
+++
@@ -1,7 +0,0 @@
-# Represents a generic neuron with basic properties and functions common to all neuron types.
-class Neuron:
-    def __init__(self, firing_rate: int):
-        self.firing_rate = firing_rate
-
-    def activate(self, stimulus: str):
-        pass
